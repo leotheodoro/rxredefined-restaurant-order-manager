82b11fe83eb64834bd935f6f7530c5f1bc5dc28d
Leonardo Theodoro <leonardo@recruitrobin.com>
2025-07-03 22:11:11 -0300

    ✨ integrate Sequelize

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import { sequelize } from "../lib/sequelize";
+
 
 export const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/health", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send({ message: 'Connection to database has been established successfully.' });
+  } catch (error) {
+    res.send({ message: `Unable to connect to the database`, error });
+  }
 });
 
