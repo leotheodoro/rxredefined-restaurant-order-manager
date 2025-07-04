81043a5040ac890b30a91d416566b25a288bdb5a
Leonardo Theodoro <leonardo@recruitrobin.com>
2025-07-04 08:26:32 -0300

    :card_file_box: Add Customer model for Sequelize ORM

---
+++ src/database/sequelize/models/customer.ts
@@ -0,0 +1,73 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import { sequelize } from "../../../lib/sequelize";
+
+interface CustomerAttributes {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+type CustomerCreationAttributes = Optional<CustomerAttributes, "id" | "createdAt" | "updatedAt">;
+
+export class Customer extends Model<CustomerAttributes, CustomerCreationAttributes>
+  implements CustomerAttributes {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public phone!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  static initialize() {
+    Customer.init(
+      {
+        id: {
+          type: DataTypes.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: true,
+          },
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true,
+            notEmpty: true,
+          },
+        },
+        phone: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: true,
+          },
+        },
+      },
+      {
+        sequelize,
+        modelName: "Customer",
+        tableName: "customers",
+        timestamps: true,
+        indexes: [
+          {
+            unique: true,
+            fields: ["email"],
+          },
+        ],
+      }
+    );
+  }
+}
+
+Customer.initialize();
