8313149cce8fac539df1e778708f9b95dcc10d00
Leonardo Theodoro <leonardo@recruitrobin.com>
2025-07-06 22:39:37 -0300

    🗃️ change env for database tests

--- src/database/sequelize/config.js
+++ src/database/sequelize/config.js
@@ -7,7 +7,7 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    use_env_variable: 'DATABASE_URL_TEST',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
   },
