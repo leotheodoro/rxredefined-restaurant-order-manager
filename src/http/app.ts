a21e1ebb39e6939e99febc563fdc7e46cdad1856
Leonardo Theodoro <leonardo@recruitrobin.com>
2025-07-07 10:47:35 -0300

    ➕ add swagger

--- src/http/app.ts
+++ src/http/app.ts
@@ -3,6 +3,8 @@ import { customersRoutes } from "./routes/customers/route";
 import { dishesRoutes } from "./routes/dishes/route";
 import { ordersRoutes } from "./routes/orders/route";
 import { errorHandler } from "./controllers/error-handler";
+import swaggerJsdoc, { Options } from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 
 export const app = express();
@@ -18,4 +20,19 @@ app.use(errorHandler)
 app.get("/", (req, res) => {
   res.send("Rx Redefined API");
 });
+
+const options: Options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'Rx Redefined API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['src/http/**/*.{ts}'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
