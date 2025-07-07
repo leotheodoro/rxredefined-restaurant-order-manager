e23ee057526e37653fa6b7a455742eea29a06f4f
Leonardo Theodoro <leonardo@recruitrobin.com>
2025-07-07 09:02:35 -0300

    :fire: remove console.logs

--- src/http/controllers/error-handler.ts
+++ src/http/controllers/error-handler.ts
@@ -6,6 +6,5 @@ export function errorHandler(err: Error, _req: Request, res: Response, _next: Ne
     return res.status(400).json({ message: 'Validation error', errors: err.flatten().fieldErrors })
   }
 
-  console.error(err.stack)
   return res.status(500).json({ message: 'Internal server error' })
 }
